048c58c04da9bdf0e23287e2e7299f9314bc5ae2
Veronika Eelmaa <veronika.eelmaa@gmail.com>
2022-06-04 14:35:22 +0300

    StringAndArrays

---
+++ FirstOrLast/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace FirstOrLast
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kasutajal sisestada ees- ja perekonnanimi
+            //programm kontrollib andmete pikkust eraldi
+            //programm kuvab kumb nendest on pikem, kas ees- või perekonnanimi
+
+            Console.WriteLine("Sisesta oma eesnimi:");
+            string firstName = Console.ReadLine();
+
+            Console.WriteLine("Sisesta oma perekonnanimi:");
+            string lastName = Console.ReadLine();
+
+            Console.WriteLine($"Sinu eesnimes on {firstName.Length} sümbolit.");
+
+            Console.WriteLine($"Sinu perekonnanimes on {lastName.Length} sümbolit.");
+
+            if (firstName.Length > lastName.Length)
+            {
+                Console.WriteLine("Sinu eesnimi on pikem.");
+            }
+            else if(firstName.Length < lastName.Length)
+            {
+                Console.WriteLine("Sinu perekonnanimi on pikem.");
+            }
+            else
+            {
+                Console.WriteLine("Sinu ees- ja perekonnanimi on ühepikkused.");
+            }
+
+            Console.WriteLine("Kena päeva!");
+        }
+    }
+}
---
+++ HelloWorldReversed/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HelloWorldReversed
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm kuvab lauset "Hello, World!" tagurpidi
+
+            string hellow = "Hello, World!";
+
+            for(int i = hellow.Length - 1; i >= 0; i--)
+            {
+                Console.Write(hellow[i]);
+            }
+            
+        }
+    }
+}
